36dda77a74eb36d20e61927490375c432bda1d5b
Michael <michael@hbar.io>
2018-05-04 18:56:30 -0400

    First commit

---
+++ src/CollapsibleHeader.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { Animated, FlatList, UIManager, findNodeHandle, Text, View } from 'react-native';
+
+export default class CollapsibleHeader extends Component {
+  state = { y: 0, height: 0 };
+
+  _onLayout = () => {
+    this._component._component.measure((x, y, width, height, pageX, pageY) => {
+      this.setState({ y: pageY, height });
+      this.props.onPosition(pageY);
+    });
+  };
+
+  render() {
+    let {
+      item,
+      expandedHeight,
+      collapsedHeight,
+      nextHeaderPosition,
+      scrollPosition,
+      yOffset,
+      contentComponent
+    } = this.props;
+
+    let inputRange = [
+      this.state.y - yOffset,
+      expandedHeight - collapsedHeight + this.state.y - yOffset
+    ];
+
+    let outputRange = [0, collapsedHeight - expandedHeight];
+
+    let transition = scrollPosition.interpolate({
+      inputRange: inputRange.slice(),
+      outputRange: [0, 1],
+      extrapolate: 'clamp'
+    });
+
+    let innerTranslateY = scrollPosition.interpolate({
+      inputRange: inputRange.slice(),
+      outputRange: [0, expandedHeight - collapsedHeight],
+      extrapolate: 'clamp'
+    });
+
+    if (nextHeaderPosition) {
+      inputRange.push(
+        nextHeaderPosition - expandedHeight - yOffset,
+        nextHeaderPosition - collapsedHeight - yOffset
+      );
+      outputRange.push(collapsedHeight - expandedHeight, 0);
+    }
+
+    let translateY = scrollPosition.interpolate({
+      inputRange,
+      outputRange,
+      extrapolate: 'clamp'
+    });
+
+    return (
+      <Animated.View
+        ref={c => (this._component = c)}
+        onLayout={this._onLayout}
+        style={{ overflow: 'hidden', transform: [{ translateY }] }}
+      >
+        <Animated.View
+          style={{ height: expandedHeight, transform: [{ translateY: innerTranslateY }] }}
+        >
+          {this.props.render({ item, transition, expandedHeight, collapsedHeight })}
+        </Animated.View>
+      </Animated.View>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { Animated, FlatList, View } from 'react-native';
+
+import produce from 'immer';
+
+import CollapsibleHeader from './CollapsibleHeader';
+
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+
+export default class CollapsibleHeaderFlatList extends Component {
+  _scrollPosition = new Animated.Value(0);
+
+  _onScroll = Animated.event([{ nativeEvent: { contentOffset: { y: this._scrollPosition } } }], {
+    useNativeDriver: true
+  });
+
+  state = { y: 0, headerPositions: {} };
+
+  _onLayout = () => {
+    this._container.measure((x, y, width, height, pageX, pageY) => {
+      this.setState({ y: pageY });
+    });
+  };
+
+  _setHeaderPosition = (i, y) => {
+    this.setState(
+      produce(({ headerPositions }) => {
+        headerPositions[i] = y;
+      })
+    );
+  };
+
+  _renderHeader = ({ item, index: i }) => {
+    let nextHeaderIndex = this.props.stickyHeaderIndices.find(h => h > i);
+    let nextHeaderPosition = this.state.headerPositions[nextHeaderIndex];
+    let { headerExpandedHeight, headerCollapsedHeight } = this.props;
+
+    return (
+      <CollapsibleHeader
+        item={item}
+        expandedHeight={headerExpandedHeight}
+        collapsedHeight={headerCollapsedHeight}
+        scrollPosition={this._scrollPosition}
+        nextHeaderPosition={nextHeaderPosition}
+        yOffset={this.state.y}
+        onPosition={y => this._setHeaderPosition(i, y)}
+        render={this.props.renderHeader}
+      />
+    );
+  };
+
+  _renderItemOrHeader = data => {
+    let isHeader = this.props.stickyHeaderIndices.includes(data.index);
+
+    if (isHeader) return this._renderHeader(data);
+
+    return this.props.renderItem(data);
+  };
+
+  render() {
+    let { renderItem, ...props } = this.props;
+    return (
+      <View ref={container => (this._container = container)} onLayout={this._onLayout}>
+        <AnimatedFlatList
+          extraData={this.state}
+          renderItem={this._renderItemOrHeader}
+          onScroll={this._onScroll}
+          {...props}
+        />
+      </View>
+    );
+  }
+}
